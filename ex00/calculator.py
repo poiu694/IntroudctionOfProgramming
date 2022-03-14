e39fde3fc9be9298d0d820e5c8767cbfc3e5d898
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-14 21:25:51 +0900

    [Feat] Add ex00 example(calculator, turtle)

---
+++ ex00/calculator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+a = int(input("첫 번째 숫자를 입력하세요: "))
+b = int(input("두 번째 숫자를 입력하세요: "))
+
+result = a + b;
+print(f'{a} + {b} = {result}')
+result = a - b;
+print(f'{a} - {b} = {result}')
+result = a * b;
+print(f'{a} * {b} = {result}')
+result = a / b;
+print(f'{a} / {b} = {result}')
